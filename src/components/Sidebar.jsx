21117c6c4a979f6d05a90618dd0cd0c62a479ff7
Anatoliy Vershinin <cata07@yandex.ru>
2018-05-21 23:59:18 +0300

    create SidebarChatList

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { withStyles } from 'material-ui/styles';
 import Drawer from 'material-ui/Drawer';
 import Divider from 'material-ui/Divider';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText'
-import Avatar from 'material-ui/Avatar';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
@@ -13,7 +9,7 @@ import BottomNavigation, { BottomNavigationAction } from 'material-ui/BottomNavi
 import ExploreIcon from '@material-ui/icons/Explore';
 import RestoreIcon from '@material-ui/icons/Restore';
 
-import titleInitials from '../utils/title-initials'
+import SidebarChatList from './SidebarChatList.jsx';
 
 const styles = theme => ({
   drawerPaper: {
@@ -43,16 +39,7 @@ const Sidebar = props => {
         <TextField fullWidth margin="normal" placeholder="Search chats..." />
       </div>
       <Divider />
-      <List className={classes.chatsList}>
-        {
-          chats.map((chat, index) => (
-            <ListItem key={index} button>
-              <Avatar>{titleInitials(chat.title)}</Avatar>
-              <ListItemText primary={chat.title}/>
-            </ListItem>
-          ))
-        }
-      </List>
+      <SidebarChatList chats={chats}/>
       <Button variant="fab" color="primary" className={classes.newChatButton}>
         <AddIcon />
       </Button>
