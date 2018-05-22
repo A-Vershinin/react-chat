70e16da3607a8e99a077f1414a50d8fdc69ee214
Anatoliy Vershinin <cata07@yandex.ru>
2018-05-22 08:46:37 +0300

    update WelcomePage

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { withStyles } from 'material-ui';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from 'material-ui/Paper';
+import Tabs, { Tab } from 'material-ui/Tabs';
 
-const WelcomePage = ({ }) => (
-  <h1>Welcome to Chat</h1>
-);
+const styles = theme => ({
+  paper: {
+    marginTop: 64 + theme.spacing.unit * 3,
+    width: 500,
+  },
+  tabContent: {
+    padding: theme.spacing.unit * 3,
+  }
+});
 
-export default WelcomePage;
+
+class WelcomePage  extends Component {
+  state = {
+    activeTab: 0,
+  };
+
+  handleTabChange = (event, value) => {
+    this.setState({ activeTab: value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { activeTab } = this.state;
+
+    return (
+      <Fragment>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="title" color="inherit" style={{ flex: 1 }}>
+              DogeCodes React Chat
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Grid container justify="center">
+          <Grid item>
+           <Paper className={classes.paper}>
+            <AppBar position="static" color="default">
+             <Tabs value={activeTab} fullWidth
+              onChange={this.handleTabChange}>
+                <Tab label="Login" />
+                <Tab label="Sign Up" />
+             </Tabs>
+            </AppBar>
+            <div className={classes.tabContent}>
+              {activeTab === 0 && <h1>Вкладка 1 </h1>}
+              {activeTab === 1 && <h1>Вкладка 2 </h1>}
+            </div>
+           </Paper>
+          </Grid>
+        </Grid>
+      </Fragment>
+    );
+  }
+
+}
+
+export default withStyles(styles)(WelcomePage);
