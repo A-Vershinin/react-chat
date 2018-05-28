3ae6ccb4943ad79b09a00f1b54ad7284f47b3ff0
Anatoliy Vershinin <cata07@yandex.ru>
2018-05-28 22:22:33 +0300

    add redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from  'redux-thunk';
+import loggerMiddleware from 'redux-logger'
 import rootReducer from '../reducers';
 
 export default function configureStore() {
-	return createStore (
-		rootReducer, applyMiddleware(thunkMiddleware)
-
-	)
+  if (process.env.NODE_ENV === 'production') {
+		return createStore (
+			rootReducer,
+			applyMiddleware(thunkMiddleware)
+		)
+	} else {
+		return createStore (
+			rootReducer,
+			applyMiddleware(thunkMiddleware, loggerMiddleware)
+		)
+	}
 }
