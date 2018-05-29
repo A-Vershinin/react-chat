25a572e8488b70f9de44bade17c3fb17fc99c90f
Anatoliy Vershinin <cata07@yandex.ru>
2018-05-29 09:38:19 +0300

    add redirect thunk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import PrivateRoute from '../containers/PrivateRoute';
 import ChatPage from '../containers/ChatPage';
 import WelcomePage from '../containers/WelcomePage';
 import configureStore from '../store/index';
+import history from '../utils/history';
 
 const store = configureStore();
 
@@ -12,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
+        <Router history={history}>
           <Switch>
             <Route exact path='/' component={WelcomePage} />
             <Route exact path='/welcome' component={WelcomePage} />
