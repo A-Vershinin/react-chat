000238757fc6b4c5f27416323c7d135f73db8d3e
Anatoliy Vershinin <cata07@yandex.ru>
2018-06-15 19:05:33 +0300

    add tests to UseMenu, WelcomePage

---
+++ src/components/UserMenu.spec.js
@@ -0,0 +1,49 @@
+/* eslint-env jest */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import UserMenu from './UserMenu.jsx';
+
+const mockProps = {
+  classes: {
+    userMenu: 'some text a',
+    modalWrapper: 'some text b',
+    modal: 'some text c',
+  },
+  onLogoutClick: jest.fn(),
+  onEditProfileClick: jest.fn(),
+  disabled: false,
+};
+
+describe('<UserMenu />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <MemoryRouter>
+        <UserMenu {...mockProps} />
+      </MemoryRouter>,
+      div,
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<MemoryRouter>
+          <UserMenu {...mockProps} />
+              </MemoryRouter>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders when UserMenu disabled', () => {
+    const tree = renderer
+      .create(<MemoryRouter>
+          <UserMenu {...mockProps} disabled />
+              </MemoryRouter>)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/components/WelcomePage.spec.js
@@ -0,0 +1,53 @@
+/* eslint-env jest */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import WelcomePage from './WelcomePage.jsx';
+
+jest.mock('./LoginForm', () => () => 'LoginForm');
+jest.mock('./SignUpForm', () => () => 'SignUpForm');
+jest.mock('./ErrorMessage', () => () => 'ErrorMessage');
+
+const mockProps = {
+  classes: {
+    paper: 'some text a',
+    tabContent: 'some text b',
+  },
+  login: jest.fn(),
+  signup: jest.fn(),
+  isAuthenticated: false,
+  error: null,
+};
+
+describe('<WelcomePage />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <MemoryRouter initialEntries={['/']}>
+        <Route path="/welcome" render={props => <WelcomePage {...mockProps} {...props} />} />
+      </MemoryRouter>,
+      div,
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<MemoryRouter initialEntries={['/']}>
+          <Route path="/welcome" render={props => <WelcomePage {...mockProps} {...props} />} />
+              </MemoryRouter>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders when user is isAuthenticated', () => {
+    const tree = renderer
+      .create(<MemoryRouter initialEntries={['/']}>
+          <Route path="/welcome" render={props => <WelcomePage {...mockProps} {...props} />} />
+              </MemoryRouter>)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
